29362a51774fa2b0aa2122e624387e23a25ea7dd
galihcandraa <galihcandrakirana17@gmail.com>
2026-09-04 09:14:54 +0700

    2.1 langkah 1

---
+++ oop-praktikum/src/main/java/id/ac/polinema/oop/praktikum/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.polinema.oop.praktikum;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.width = 6;
+        r.height = 4;
+        
+        System.out.println("Rectangle " + r.width + "x" + r.height);
+    }
+}
---
+++ oop-praktikum/src/main/java/id/ac/polinema/oop/praktikum/Rectangle.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.polinema.oop.praktikum;
+
+/**
+ *
+ * @author user
+ */
+public class Rectangle {
+    int width;
+    int height;
+}
